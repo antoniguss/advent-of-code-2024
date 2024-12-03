6e7b52d4d3b524c2ddd8ef649b7520e102f4ce18
Antoni Guss <antoniguss0@gmail.com>
2024-12-03 16:05:01 +0100

    Completed day2 part2

--- solutions/day3/solution.go
+++ solutions/day3/solution.go
@@ -23,24 +23,65 @@ func main() {
 		inputString += line
 	}
 
-	//IDEA: Use regex, something like 'mul(%d,%d)' to accept a mul command
-	regex, err := regexp.Compile("mul\\([0-9]+,[0-9]+\\)")
+	//IDEA: Use mulRegex, something like 'mul(%d,%d)' to accept a mul command
+	mulRegex, err := regexp.Compile("mul\\([0-9]+,[0-9]+\\)")
 	check(err)
 
-	allString := regex.FindAllString(inputString, -1)
+	allString := mulRegex.FindAllString(inputString, -1)
 	sum := 0
 	for _, operation := range allString {
-		regex = regexp.MustCompile("\\((\\d+),(\\d+)\\)")
-		submatch := regex.FindStringSubmatch(operation)
-		n1, err := strconv.Atoi(submatch[1])
-		check(err)
-		n2, err := strconv.Atoi(submatch[2])
-		check(err)
-
-		sum += n1 * n2
+		sum += processMul(operation)
 	}
 
 	fmt.Printf("The sum of all operations is: %d\n", sum)
+
+	//--- Part 2 ---
+	sum2 := 0
+	doRegex := regexp.MustCompile("do\\(\\)")
+	dontRegex := regexp.MustCompile("don't\\(\\)")
+	doIndices := doRegex.FindAllStringIndex(inputString, -1)
+	dontIndices := dontRegex.FindAllStringIndex(inputString, -1)
+	mulIndicies := mulRegex.FindAllStringIndex(inputString, -1)
+
+	fmt.Println(doIndices)
+	fmt.Println(dontIndices)
+	fmt.Println(mulIndicies)
+	do := true
+	for i := range len(inputString) {
+		if len(doIndices) > 0 && doIndices[0][0] == i {
+			do = true
+			doIndices = append(doIndices[:0], doIndices[1:]...)
+		}
+
+		if len(dontIndices) > 0 && dontIndices[0][0] == i {
+			do = false
+			dontIndices = append(dontIndices[:0], dontIndices[1:]...)
+		}
+
+		if len(mulIndicies) > 0 && mulIndicies[0][0] == i {
+			mulIndex := mulIndicies[0]
+			if do {
+				mulString := inputString[mulIndex[0]:mulIndex[1]]
+				sum2 += processMul(mulString)
+			}
+
+			mulIndicies = append(mulIndicies[:0], mulIndicies[1:]...)
+
+		}
+	}
+
+	fmt.Printf("The sum of all operations is: %d\n", sum2)
+
+}
+
+func processMul(operation string) int {
+	parameterRegex := regexp.MustCompile("\\((\\d+),(\\d+)\\)")
+	submatch := parameterRegex.FindStringSubmatch(operation)
+	n1, err := strconv.Atoi(submatch[1])
+	check(err)
+	n2, err := strconv.Atoi(submatch[2])
+	check(err)
+	return n1 * n2
 }
 
 func check(e error) {
