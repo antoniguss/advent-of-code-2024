cefb283ce540ddfe21b6c0c63571d8f4d2a5fcc0
Antoni Guss <antoniguss0@gmail.com>
2024-12-22 16:17:02 +0100

    Completed day19 part2

--- solutions/day19/solution.go
+++ solutions/day19/solution.go
@@ -41,26 +41,36 @@ func main() {
 	fmt.Println("Advent of Code - Day 19") // Placeholder for day number
 
 	inputPath := "./input.txt"
-	towels, wanted, longest := input(inputPath)
+	towels, wanted, _ := input(inputPath)
 
 	// fmt.Println(towels, wanted)
 
 	//--- Part 1 ---
-	sol1 := part1(towels, wanted, longest)
+	sol1 := part1(towels, wanted)
 	fmt.Printf("Part1: %d\n", sol1)
 
 	//--- Part 2 ---
-	// sol2 := part2(towels, wanted)
-	// fmt.Printf("Part2: %d\n", sol2)
+	sol2 := part2(towels, wanted)
+	fmt.Printf("Part2: %d\n", sol2)
 
 }
 
-func part2(towels, wanted []string) (count int) {
+func part2(towels []string, wanted map[string]struct{}) (count int) {
+	made := make(map[string]int)
 
+	for pattern := range wanted {
+		// fmt.Println(pattern)
+		// fmt.Printf("pattern: %v\n", pattern)
+		ways := canMakeWays(towels, pattern, made)
+		// fmt.Println(ways)
+		count += ways
+
+	}
 	return count
+
 }
 
-func part1(towels []string, wanted map[string]struct{}, longest int) (count int) {
+func part1(towels []string, wanted map[string]struct{}) (count int) {
 	made := make(map[string]struct{})
 	notMade := make(map[string]struct{})
 
@@ -101,6 +111,38 @@ func canMake(towels []string, pattern string, made, notMade map[string]struct{})
 
 	notMade[pattern] = struct{}{}
 	return false
+
+}
+
+func canMakeWays(towels []string, pattern string, made map[string]int) (count int) {
+	if len(pattern) == 0 {
+		return 1
+	}
+
+	if made[pattern] == -1 {
+		return 0
+	}
+
+	if ways, has := made[pattern]; has {
+		return ways
+	}
+	// fmt.Println(len(made))
+
+	for _, towel := range towels {
+		// fmt.Printf("towel: %v\n", towel)
+		if len(pattern) >= len(towel) && towel == pattern[:len(towel)] {
+			count += canMakeWays(towels, pattern[len(towel):], made)
+		}
+
+	}
+	if count == 0 {
+		made[pattern] = -1
+	} else {
+		made[pattern] = count
+	}
+	// fmt.Println("Coulnd't")
+
+	return count
 
 }
 
