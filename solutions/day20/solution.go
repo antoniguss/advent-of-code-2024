a881c7b1a9a7df66b9f2a894c8ca5a89a7cd958d
Antoni Guss <antoniguss0@gmail.com>
2024-12-23 13:37:15 +0100

    Completed day20 part2

--- solutions/day20/solution.go
+++ solutions/day20/solution.go
@@ -3,14 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
-
 	"github.com/Antyhot/advent-of-code-24/util"
+	"github.com/Antyhot/advent-of-code-24/util/queue"
+	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Advent of Code - Day 20") // Placeholder for day number
 
+	start, end, maze := getInput()
+
+	//--- Part 1 ---
+	startTime1 := time.Now() // Start time for Part 1
+	var sol1 int = part1(start, end, maze)
+	elapsedTime1 := time.Since(startTime1) // Calculate elapsed time for Part 1
+	fmt.Printf("Part1: %d (Time: %s)\n", sol1, elapsedTime1)
+
+	startTime2 := time.Now() // Start time for Part 2
+	var sol2 int = part2(start, end, maze)
+	elapsedTime2 := time.Since(startTime2) // Calculate elapsed time for Part 2
+	fmt.Printf("Part2: %d (Time: %s)\n", sol2, elapsedTime2)
+
+}
+
+func getInput() (util.Vector, util.Vector, [][]bool) {
 	file, err := os.Open("input.txt")
 	check(err)
 	scanner := bufio.NewScanner(file)
@@ -23,7 +40,6 @@ func main() {
 	rows := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
 		mazeLine := make([]bool, len(line))
 		for i, c := range line {
 			switch c {
@@ -42,18 +58,7 @@ func main() {
 		maze = append(maze, mazeLine)
 		rows++
 	}
-
-	//--- Part 1 ---
-	var sol1 int = part1(start, end, maze)
-	fmt.Printf("Part1: %d\n", sol1)
-	//--- Part 2 ---
-	var sol2 int = part2()
-	fmt.Printf("Part2: %d\n", sol2)
-
-	//--- Cleanup ---
-	err = file.Close()
-	check(err)
-
+	return start, end, maze
 }
 
 type maze [][]bool
@@ -65,10 +70,8 @@ func (m maze) get(pos util.Vector) bool {
 	return true
 }
 
-func part1(start, end util.Vector, m maze) (cheatCount int) {
-	//fmt.Println(start, end)
-	distance := make(map[util.Vector]int)
-
+func calcDistances(m maze, end, start util.Vector) (distance map[util.Vector]int) {
+	distance = make(map[util.Vector]int)
 	cur := start
 	distance[cur] = 0
 	var prev util.Vector
@@ -86,8 +89,93 @@ func part1(start, end util.Vector, m maze) (cheatCount int) {
 			}
 		}
 	}
+	return distance
+}
 
-	cur = start
+type pair struct {
+	util.Vector
+	distance int
+}
+
+// Solution using BFS, worked in ~2s
+// Could be better if we only examined points on the path (since the start and end of a cheat has to be on the path)
+func part2(start, end util.Vector, m maze) (cheatCount int) {
+	//fmt.Println(start, end)
+
+	distance := calcDistances(m, end, start)
+
+	cur := start
+	prev := util.Vector{}
+	for cur != end {
+		curDist, _ := distance[cur]
+
+		maxSkipLen := 20
+		cheatThresh := 100
+		//Start a BFS search to check all positions within 20 picoseconds
+		checked := map[util.Vector]struct{}{}
+
+		queue := queue.NewQueue()
+		queue.Enqueue(pair{
+			cur,
+			0,
+		})
+		for !queue.IsEmpty() {
+			cheat := queue.Dequeue().(pair)
+
+			if _, has := checked[cheat.Vector]; has {
+				continue
+			}
+
+			if cheatDist, has := distance[cheat.Vector]; has && cheatDist > curDist {
+				skip := cheatDist - curDist - cheat.distance
+				if skip >= cheatThresh {
+					cheatCount++
+				}
+
+			}
+			checked[cheat.Vector] = struct{}{}
+
+			//Add other possible options
+			options := []pair{
+				{cheat.Vector.Above(1), cheat.distance + 1},
+				{cheat.Vector.Right(1), cheat.distance + 1},
+				{cheat.Vector.Below(1), cheat.distance + 1},
+				{cheat.Vector.Left(1), cheat.distance + 1},
+			}
+
+			for _, opt := range options {
+				if opt.distance > maxSkipLen {
+					continue
+				}
+
+				if _, has := checked[opt.Vector]; has {
+					continue
+				}
+				queue.Enqueue(opt)
+			}
+		}
+
+		//Get the next step
+		options := []util.Vector{cur.Above(1), cur.Below(1), cur.Left(1), cur.Right(1)}
+		for _, opt := range options {
+			if !m.get(opt) && opt != prev {
+				prev = cur
+				cur = opt
+				break
+			}
+		}
+	}
+
+	return cheatCount
+}
+
+func part1(start, end util.Vector, m maze) (cheatCount int) {
+	//fmt.Println(start, end)
+
+	distance := calcDistances(m, end, start)
+
+	cur := start
+	var prev util.Vector
 
 	for cur != end {
 		curDist, _ := distance[cur]
@@ -150,10 +238,6 @@ func part1(start, end util.Vector, m maze) (cheatCount int) {
 	}
 
 	return cheatCount
-}
-
-func part2() (cheats int) {
-	return cheats
 }
 
 func check(e error) {
